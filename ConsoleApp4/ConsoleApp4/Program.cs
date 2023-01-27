940505e02f719c01056c463ebb02bce78fed874f
Radosław Niemczyk <radoslaw.niemczyk@stark.dk>
2023-01-27 11:36:25 +0100

    init

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        private static ActivitySource source = new ActivitySource("Sample.DistributedTracing", "1.0.0");
+
+        static async Task Main(string[] args)
+        {
+            var counter = 0;
+            while (true)
+            {
+                await DoSomeWork("banana", 8);
+                Console.WriteLine("Example work done:" + counter);
+                counter++;
+            }
+        }
+
+        // All the functions below simulate doing some arbitrary work
+        static async Task DoSomeWork(string foo, int bar)
+        {
+            using (Activity activity = source.StartActivity("SomeWork"))
+            {
+                activity?.SetTag("foo", foo);
+                activity?.SetTag("bar", bar);
+                await StepOne();
+                await StepTwo();
+            }
+        }
+
+        static async Task StepOne()
+        {
+            using (Activity activity = source.StartActivity("StepOne"))
+            {
+                await Task.Delay(500);
+            }
+        }
+
+        static async Task StepTwo()
+        {
+            using (Activity activity = source.StartActivity("StepTwo"))
+            {
+                await Task.Delay(1000);
+            }
+        }
+    }
+}
